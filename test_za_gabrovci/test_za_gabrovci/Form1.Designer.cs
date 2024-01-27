405c24488c0f9a3e32760fdac92f5f11771422cd
Nataliq <nat.g.dimitrova@gmail.com>
2024-01-27 17:39:25 +0200

    Badge window 5:39 pm 27/01/2024

---
+++ test_za_gabrovci/test_za_gabrovci/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace test_za_gabrovci
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.badge_lbl = new System.Windows.Forms.Label();
+            this.name_panel = new System.Windows.Forms.Panel();
+            this.content_panel = new System.Windows.Forms.Panel();
+            this.game1 = new System.Windows.Forms.PictureBox();
+            this.game2 = new System.Windows.Forms.PictureBox();
+            this.game3 = new System.Windows.Forms.PictureBox();
+            this.game6 = new System.Windows.Forms.PictureBox();
+            this.game5 = new System.Windows.Forms.PictureBox();
+            this.game4 = new System.Windows.Forms.PictureBox();
+            this.help_btn = new System.Windows.Forms.PictureBox();
+            this.name_panel.SuspendLayout();
+            this.content_panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.game1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.game2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.game3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.game6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.game5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.game4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.help_btn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // badge_lbl
+            // 
+            this.badge_lbl.AutoSize = true;
+            this.badge_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.badge_lbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(93)))), ((int)(((byte)(4)))));
+            this.badge_lbl.Location = new System.Drawing.Point(267, 9);
+            this.badge_lbl.Name = "badge_lbl";
+            this.badge_lbl.Size = new System.Drawing.Size(653, 95);
+            this.badge_lbl.TabIndex = 0;
+            this.badge_lbl.Text = "Бадж Колекция";
+            this.badge_lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // name_panel
+            // 
+            this.name_panel.Controls.Add(this.help_btn);
+            this.name_panel.Controls.Add(this.badge_lbl);
+            this.name_panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.name_panel.Location = new System.Drawing.Point(0, 0);
+            this.name_panel.Name = "name_panel";
+            this.name_panel.Size = new System.Drawing.Size(1182, 119);
+            this.name_panel.TabIndex = 1;
+            // 
+            // content_panel
+            // 
+            this.content_panel.Controls.Add(this.game6);
+            this.content_panel.Controls.Add(this.game5);
+            this.content_panel.Controls.Add(this.game4);
+            this.content_panel.Controls.Add(this.game3);
+            this.content_panel.Controls.Add(this.game2);
+            this.content_panel.Controls.Add(this.game1);
+            this.content_panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.content_panel.Location = new System.Drawing.Point(0, 119);
+            this.content_panel.Name = "content_panel";
+            this.content_panel.Size = new System.Drawing.Size(1182, 584);
+            this.content_panel.TabIndex = 2;
+            // 
+            // game1
+            // 
+            this.game1.BackColor = System.Drawing.Color.White;
+            this.game1.Location = new System.Drawing.Point(141, 35);
+            this.game1.Name = "game1";
+            this.game1.Size = new System.Drawing.Size(211, 211);
+            this.game1.TabIndex = 4;
+            this.game1.TabStop = false;
+            // 
+            // game2
+            // 
+            this.game2.BackColor = System.Drawing.Color.White;
+            this.game2.Location = new System.Drawing.Point(493, 35);
+            this.game2.Name = "game2";
+            this.game2.Size = new System.Drawing.Size(211, 211);
+            this.game2.TabIndex = 5;
+            this.game2.TabStop = false;
+            // 
+            // game3
+            // 
+            this.game3.BackColor = System.Drawing.Color.White;
+            this.game3.Location = new System.Drawing.Point(846, 35);
+            this.game3.Name = "game3";
+            this.game3.Size = new System.Drawing.Size(211, 211);
+            this.game3.TabIndex = 6;
+            this.game3.TabStop = false;
+            // 
+            // game6
+            // 
+            this.game6.BackColor = System.Drawing.Color.White;
+            this.game6.Location = new System.Drawing.Point(846, 305);
+            this.game6.Name = "game6";
+            this.game6.Size = new System.Drawing.Size(211, 211);
+            this.game6.TabIndex = 9;
+            this.game6.TabStop = false;
+            // 
+            // game5
+            // 
+            this.game5.BackColor = System.Drawing.Color.White;
+            this.game5.Location = new System.Drawing.Point(493, 305);
+            this.game5.Name = "game5";
+            this.game5.Size = new System.Drawing.Size(211, 211);
+            this.game5.TabIndex = 8;
+            this.game5.TabStop = false;
+            // 
+            // game4
+            // 
+            this.game4.BackColor = System.Drawing.Color.White;
+            this.game4.Location = new System.Drawing.Point(141, 305);
+            this.game4.Name = "game4";
+            this.game4.Size = new System.Drawing.Size(211, 211);
+            this.game4.TabIndex = 7;
+            this.game4.TabStop = false;
+            // 
+            // help_btn
+            // 
+            this.help_btn.BackColor = System.Drawing.Color.White;
+            this.help_btn.Location = new System.Drawing.Point(1077, 12);
+            this.help_btn.Name = "help_btn";
+            this.help_btn.Size = new System.Drawing.Size(93, 93);
+            this.help_btn.TabIndex = 5;
+            this.help_btn.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(7)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(1182, 703);
+            this.Controls.Add(this.content_panel);
+            this.Controls.Add(this.name_panel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Бадж колекция";
+            this.name_panel.ResumeLayout(false);
+            this.name_panel.PerformLayout();
+            this.content_panel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.game1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.game2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.game3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.game6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.game5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.game4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.help_btn)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label badge_lbl;
+        private System.Windows.Forms.Panel name_panel;
+        private System.Windows.Forms.Panel content_panel;
+        private System.Windows.Forms.PictureBox help_btn;
+        private System.Windows.Forms.PictureBox game6;
+        private System.Windows.Forms.PictureBox game5;
+        private System.Windows.Forms.PictureBox game4;
+        private System.Windows.Forms.PictureBox game3;
+        private System.Windows.Forms.PictureBox game2;
+        private System.Windows.Forms.PictureBox game1;
+    }
+}
+
